275c061162b5e539d4351ea500d28e0ae23cc34a
davidvaldh <ds.valderrama@uniandes.edu.co>
2023-05-27 16:42:10 -0500

    Revert "Postgresql connection set up"
    This reverts commit 62a9c0073425c6b3fc42967e094285e45572a7e1.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using WidmyIPS.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
-builder.Services.AddDbContext<IPSDb>(options => options.UseNpgsql(connectionString));
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
