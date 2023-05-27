62a9c0073425c6b3fc42967e094285e45572a7e1
davidvaldh <ds.valderrama@uniandes.edu.co>
2023-05-27 16:16:22 -0500

    Postgresql connection set up

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WidmyIPS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
+builder.Services.AddDbContext<IPSDb>(options => options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
